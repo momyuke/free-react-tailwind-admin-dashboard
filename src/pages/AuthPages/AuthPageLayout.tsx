ca2c1a44270775aaf51ab3f48c58a7d84b14b4ba
RaflyUr <raflyur506@gmail.com>
2025-05-15 21:15:31 +0700

    Change icon to be idealab logo

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -21,13 +21,12 @@ export default function AuthLayout({
                 <img
                   width={231}
                   height={48}
-                  src="/images/logo/auth-logo.svg"
+                  src="/images/logo/idealab-logoonly.png"
                   alt="Logo"
                 />
               </Link>
               <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
+              Creating product that are a joy to use and lovely to behold!              </p>
             </div>
           </div>
         </div>
